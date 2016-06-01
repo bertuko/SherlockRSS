128b4bcee7c9a77f94261907a21860aa31c3a2a6
Alberto <bertuko@gmail.com>
2016-06-01 09:57:58 +0200

    onclick event

---
+++ app/src/main/java/com/albertbaron/sherlockrss/activities/ArticleActivity.kt
@@ -0,0 +1,29 @@
+package com.albertbaron.sherlockrss.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import com.albertbaron.sherlockrss.R
+import com.albertbaron.sherlockrss.models.Article
+
+class ArticleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+        val art: Article = Article(
+                intent.getStringExtra("Title"),
+                intent.getStringExtra("Author"),
+                intent.getStringExtra("Description"),
+                intent.getStringExtra("Link"),
+                intent.getStringExtra("ImageLink"),
+                null
+        )
+        val wv: WebView = findViewById(R.id.webView) as WebView
+        wv.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING
+        val head: String = "<head><style>img{max-width: 100%; width:auto; height: auto;}</style></head>";
+        val code :String= "<html>" + head + "<body>" + art.Description + "</body></html>"
+        wv.loadData(code, "text/html", null)
+    }
+}
