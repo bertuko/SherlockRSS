5712d5ea4878f3665a209b8d0193874db718f642
Alberto <bertuko@gmail.com>
2016-06-04 13:15:23 +0200

    first save of feeds info

--- app/src/main/java/com/albertbaron/sherlockrss/activities/MainActivity.kt
+++ app/src/main/java/com/albertbaron/sherlockrss/activities/MainActivity.kt
@@ -9,6 +9,11 @@ import com.albertbaron.sherlockrss.R
 import com.albertbaron.sherlockrss.adapters.FeedListAdapter
 import com.albertbaron.sherlockrss.helpers.FeedHelper
 import com.albertbaron.sherlockrss.models.*
+import com.raizlabs.android.dbflow.config.FlowManager
+import com.raizlabs.android.dbflow.kotlinextensions.processInTransactionAsync
+import com.raizlabs.android.dbflow.sql.language.SQLite
+import com.raizlabs.android.dbflow.structure.database.transaction.FastStoreModelTransaction
+import com.raizlabs.android.dbflow.structure.database.transaction.Transaction
 //import com.albertbaron.sherlockrss.layouts.MainActivityUI
 import org.jetbrains.anko.*
 import java.util.*
@@ -28,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun fillMainActivity (al: ArrayList<ArticleList>) {
-        longToast("feeds updated")
+        //longToast("feeds updated")
         //val mListView: RecyclerView = ui.holder!!.feedView
         val mListView: RecyclerView =  findViewById(R.id.feedList) as RecyclerView
         mListView.adapter = FeedListAdapter(al, R.layout.feed_list)
@@ -38,19 +43,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun getFeedList(): ArrayList<FeedInfo> {
-        val feeds: ArrayList<FeedInfo> = ArrayList<FeedInfo>()
-        feeds.add(FeedInfo("xataka", "http://www.xataka.com/index.xml"))
-        feeds.add(FeedInfo("xatakamovil", "http://www.xatakamovil.com/index.xml"))
-        feeds.add(FeedInfo("xatakafoto", "http://www.xatakafoto.com/index.xml"))
-        feeds.add(FeedInfo("xatakandroid", "http://www.xatakandroid.com/index.xml"))
-        feeds.add(FeedInfo("xatakahome", "http://www.xatakahome.com/index.xml"))
-        feeds.add(FeedInfo("xatakawindows", "http://www.xatakawindows.com/index.xml"))
-        feeds.add(FeedInfo("xatakaciencia", "http://www.xatakaciencia.com/index.xml"))
-        feeds.add(FeedInfo("applesfera", "http://www.applesfera.com/index.xml"))
-        feeds.add(FeedInfo("vidaextra", "http://www.vidaextra.com/index.xml"))
-        feeds.add(FeedInfo("genbeta", "http://www.genbeta.com/index.xml"))
-        feeds.add(FeedInfo("genbetadev", "http://www.genbetadev.com/index.xml"))
-        return feeds
+        val feeds: MutableList<FeedInfo> = SQLite.select()
+                .from(FeedInfo::class.java)
+                .orderBy(FeedInfo_Table.id, true)
+                .queryList()
+
+        if (feeds.count() == 0){
+            feeds.add(FeedInfo("xataka", "http://www.xataka.com/index.xml"))
+            feeds.add(FeedInfo("xatakamovil", "http://www.xatakamovil.com/index.xml"))
+            feeds.add(FeedInfo("xatakafoto", "http://www.xatakafoto.com/index.xml"))
+            feeds.add(FeedInfo("xatakandroid", "http://www.xatakandroid.com/index.xml"))
+            feeds.add(FeedInfo("xatakahome", "http://www.xatakahome.com/index.xml"))
+            feeds.add(FeedInfo("xatakawindows", "http://www.xatakawindows.com/index.xml"))
+            feeds.add(FeedInfo("xatakaciencia", "http://www.xatakaciencia.com/index.xml"))
+            feeds.add(FeedInfo("applesfera", "http://www.applesfera.com/index.xml"))
+            feeds.add(FeedInfo("vidaextra", "http://www.vidaextra.com/index.xml"))
+            feeds.add(FeedInfo("genbeta", "http://www.genbeta.com/index.xml"))
+            feeds.add(FeedInfo("genbetadev", "http://www.genbetadev.com/index.xml"))
+
+            feeds.processInTransactionAsync({ it, databaseWrapper -> it.save(databaseWrapper) }
+                    /*,Transaction.Success {
+                        longToast("feeds saved")
+                    }
+                    ,Transaction.Error {transaction, throwable ->
+                        longToast("feeds save error")
+                    }*/
+            )
+        }
+
+        return feeds as ArrayList<FeedInfo>
     }
 
 }
--- app/src/main/java/com/albertbaron/sherlockrss/models/Database.kt
+++ app/src/main/java/com/albertbaron/sherlockrss/models/Database.kt
@@ -2,7 +2,7 @@ package com.albertbaron.sherlockrss.models
 
 import com.raizlabs.android.dbflow.annotation.Database
 
-@Database(name = MyDatabase.NAME, version = MyDatabase.VERSION, generatedClassSeparator = "$")
+@Database(name = MyDatabase.NAME, version = MyDatabase.VERSION, generatedClassSeparator = "_")
 object MyDatabase {
     const val NAME: String = "MyDatabase"
     const val VERSION: Int = 1
